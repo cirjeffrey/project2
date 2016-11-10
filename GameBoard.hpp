c8d2c2ebaa334277b7eac292488a103a3a66a946
cirjeffrey <cirjeffrey92@gmail.com>
2016-11-10 13:23:53 -0800

    Final update

--- GameBoard.hpp
+++ GameBoard.hpp
@@ -12,18 +12,9 @@ public:
 	enum Ladders { ONE = 1, FOUR = 4, NINE = 9, TWENTY_ONE = 21, TWENTY_EIGHT = 28, THIRTY_SIX = 36, FIFTY_ONE = 51, SEVENTY_ONE = 71, EIGHTY = 80 };
 	enum Chutes { SIXTEEN = 16, FORTY_SEVEN = 47, FORTY_NINE = 49, FIFTY_SIX = 56, SIXTY_TWO = 62, SIXTY_FOUR = 64, EIGHTY_SEVEN = 87, NINETY_THREE = 93, NINETY_FIVE = 95, NINETY_EIGHT = 98 };
 
-	// Build the gameboard
-	// TO DO: implement this function
 	void buildBoard();
+	GameBoard() {buildBoard();}
 
-	GameBoard() {
-
-		buildBoard();
-	}
-	// If player lands on chutes or ladders, returns the new position.
-	// Otherwise, returns the player's current position.
-	// If the player's position is outside of the gameboard, throws index out of bounds exception
-	// TO DO: implement this function properly
 	int checkChutesLadders(int position) {
 		if ((position < 0) || (position >= BOARD_SIZE)) {
 			throw range_error("index out of bounds");
