df395245308bbd5b8b3dfb987a4dc6d68837a1f8
cirjeffrey <cirjeffrey92@gmail.com>
2016-11-10 13:23:31 -0800

    Final Update

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -4,13 +4,6 @@
 
 #include "GameBoard.hpp"
 
-// Build the gameboard
-// If the landing square is a chute, set the position to the corresponding lower square
-// I.e., player slides down to the lower square
-// If the landing square is a ladder, set the position to the corresponding higher square
-// Player climbs up to the higher square
-// Otherwise, player stays put at the landing square
-// TO DO: implement this method
 void GameBoard::buildBoard(){
 		for (int i = 0; i < 100; i++)
 			vector.insert(i, i + 1);		
