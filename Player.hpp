c828f665e4ca321a5c565ef59939bede33db7ef7
cirjeffrey <cirjeffrey92@gmail.com>
2016-11-10 13:25:02 -0800

    Final Update

--- Player.hpp
+++ Player.hpp
@@ -6,10 +6,8 @@
 
 #include <stdio.h>
 #include <string>
-
 #include "Die.hpp"
 #include "GameBoard.hpp"
-
 using namespace std;
 
 class Player {
@@ -23,20 +21,13 @@ public:
 	
 	~Player(){}
 
-	// getters and setters
-
 	void setName(string name) { playerName = name; }
-	void setPostion(int location) { position = location; }
+	void setPosition(int location) { position = location; }
 
 	string getName() const { return playerName; }
-	int getPostion() const { return position; }
+	int getPosition() const { return position; }
 	Die getDie() { return die; }
 
-	// return player's new position after player rolls die
-	// If the new position is outside of the board, player stays put
-	// If not, player moves to the new square = player's postion + die's face value
-	// TO DO: implement this function to move player to the new postion
-	//        after player rolls die. Position must be inside the board
 	int rollDieAndMove();
 	
 private:
