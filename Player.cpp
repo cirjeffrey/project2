6c1cc71308a41747b25fd494254e23f684a1cf0f
cirjeffrey <cirjeffrey92@gmail.com>
2016-11-10 13:24:23 -0800

    Final update

--- Player.cpp
+++ Player.cpp
@@ -1,28 +1,37 @@
 //
-//  Player.cpp
+//  Player.hpp
 //
 
-#include "Player.hpp"
+#pragma once
 
-// TO DO: implement Player's assignment operator
-Player& Player::operator=(const Player& p) {
-	playerName = p.playerName;
-	position = p.position;
-	die = p.die;
-	return *this;
-}
+#include <stdio.h>
+#include <string>
+#include "Die.hpp"
+#include "GameBoard.hpp"
+using namespace std;
 
-// return player's new position after player rolls die
-// If the new position is outside of the board, player stays put
-// If not, player moves to the new square = player's postion + die's face value
-// TO DO: implement this function to move player to the new postion
-//        after player rolls die. Position must be inside the board
-int Player::rollDieAndMove() {
+class Player {
 
-	die.roll();
-	position = position + die.getFaceValue();
-	if ((position < 0) || (position >= BOARD_SIZE)) {
-		throw range_error("index out of bounds");
-	}
-	return position;
-}
+public:
+	Player() : playerName("no name"), position(0), die() {}
+	Player(string name) : playerName(name), position(0), die() {}
+	Player(const Player &p) : playerName(p.playerName), position(p.position) {}
+	
+	Player& operator=(const Player& p);
+	
+	~Player(){}
+
+	void setName(string name) { playerName = name; }
+	void setPosition(int location) { position = location; }
+
+	string getName() const { return playerName; }
+	int getPosition() const { return position; }
+	Die getDie() { return die; }
+
+	int rollDieAndMove();
+	
+private:
+	string playerName;
+	int position;
+	Die die;
+};
