84db4504559f1d6659944e6962e95e99bf2adc30
cirjeffrey <cirjeffrey92@gmail.com>
2016-11-10 13:25:33 -0800

    Final update

--- Player.cpp
+++ Player.cpp
@@ -1,37 +1,25 @@
 //
-//  Player.hpp
+//  Player.cpp
 //
 
-#pragma once
+#include "Player.hpp"
 
-#include <stdio.h>
-#include <string>
-#include "Die.hpp"
-#include "GameBoard.hpp"
-using namespace std;
 
-class Player {
+Player& Player::operator=(const Player& p) {
+	playerName = p.playerName;
+	position = p.position;
+	die = p.die;
+	return *this;
+}
 
-public:
-	Player() : playerName("no name"), position(0), die() {}
-	Player(string name) : playerName(name), position(0), die() {}
-	Player(const Player &p) : playerName(p.playerName), position(p.position) {}
-	
-	Player& operator=(const Player& p);
-	
-	~Player(){}
+int Player::rollDieAndMove() {
 
-	void setName(string name) { playerName = name; }
-	void setPosition(int location) { position = location; }
+	die.roll();
+	position = position + die.getFaceValue();
+	if (position < 0)
+		throw range_error("index out of bounds");
+	else if (position >= BOARD_SIZE)
+		position = position - die.getFaceValue();
 
-	string getName() const { return playerName; }
-	int getPosition() const { return position; }
-	Die getDie() { return die; }
-
-	int rollDieAndMove();
-	
-private:
-	string playerName;
-	int position;
-	Die die;
-};
+	return position;
+}
